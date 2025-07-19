68222bbb542d76e2a31f484639ae55b3e0e31dc6
DaritoElinge <dpincayp@unemi.edu.ec>
2025-07-19 12:30:52 -0500

    Fix: eliminar vercel.json y ajustar funcion serveless

--- api/weather/[city].js
+++ api/weather/[city].js
@@ -1,19 +1,17 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const {
-    query: { city }
-  } = req;
-
+  const { city } = req.query;
   const apiKey = process.env.VITE_API_KEY;
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`;
 
   try {
     const response = await fetch(url);
-    if (!response.ok) throw new Error('City not found');
+    if (!response.ok) throw new Error('Ciudad no encontrada');
     const data = await response.json();
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
